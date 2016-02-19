4c7c7fba7b668f1e0d787ffd593c0e9af2542ac4
masogit <shuo.man@gmail.com>
2016-02-19 13:21:39 +0800

    remove unused routers

--- app/routes.js
+++ app/routes.js
@@ -69,45 +69,6 @@ module.exports = function (app, rds_client) {
 
     }
 
-    // CMS REST -------------------------------------------------------------
-    app.post('/cms/get', function (req, res) {
-        var server = "http://" + req.body.server;
-        var args = {
-            data: req.body.view, // TQL Name,
-            headers: { "Content-Type": "text/plain" },
-            requestConfig: { timeout: 2000 },
-            responseConfig: { timeout: 5000 }
-        };
-
-        var req = client.post(server + "/rest/topology/", args, function (data, response) {
-            res.json(data);
-        });
-
-        req.on('error', function (err) {
-            console.log('request error');
-        });
-
-    });
-
-    app.post('/cms/post', function (req, res) {
-        var server = "http://" + req.body.server;
-        var args = {
-            data: req.body.data, // UCMDB CIs,
-            headers: { "Content-Type": "application/json" },
-            requestConfig: { timeout: 2000 },
-            responseConfig: { timeout: 5000 }
-        };
-
-        var req = client.post(server + "/rest/dataIn", args, function (data, response) {
-            console.log('ucmdb create/update data: ' + data);
-            res.send(data);
-        });
-        console.log('req.options: ' + JSON.stringify(req.options));
-        req.on('error', function (err) {
-            console.log('request error');
-        });
-
-    });
     // AM Metadata ---------------------------------------------------------
     app.post('/am/metadata', function (req, res) {
         // all Table = "metadata/tables";
@@ -226,45 +187,6 @@ module.exports = function (app, rds_client) {
 
 
     });
-
-    // SSH CMD -------------------------------------------------------------
-    app.post('/ssh/exec', function (req, res) {
-        console.log("req.body.cmd: " + req.body.cmd);
-        var ssh = new SSH({
-            host: req.body.host,
-            user: req.body.user,
-            pass: req.body.pass
-        });
-
-        ssh.exec(req.body.cmd, {
-            out: function (stdout) {
-                console.log("stdout: " + stdout);
-                res.send(stdout);
-            },
-
-            err: function (stderr) {
-                res.send(stderr);
-                console.log("stderr: " + stderr);
-            }
-        }).start();
-    });
-
-    // load the single view file
-    app.get('/cfg', function (req, res) {
-        res.sendfile('./public/cfg.html');
-    });
-
-    app.get('/ssh', function (req, res) {
-        res.sendfile('./public/ssh.html');
-    });
-
-    app.get('/cms', function (req, res) {
-        res.sendfile('./public/cms.html');
-    });
-
-    //    app.get('/am', function (req, res) {
-    //        res.sendfile('./public/am.html');
-    //    });
 
     app.get('/amx', function (req, res) {
         res.sendfile('./public/browser/amx.html');
