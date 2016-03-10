712a017f24cd47f4d9f2aa7f822c9cc7fedf1402
Man, Shuo <shuo.man@hpe.com>
2016-03-10 19:23:02 +0800

    sub process for caching

--- app/cache.js
+++ app/cache.js
@@ -1,7 +1,3 @@
-var intervalObject;
-var Client = require('node-rest-client').Client;
-var client = new Client();
-
 module.exports = function (db, am, redisConf) {
     // init redis search client, need redis service on 127.0.0.1:6379
     var redis = require("redis"),
@@ -25,31 +21,6 @@ module.exports = function (db, am, redisConf) {
         client: rds_client      // The redis client instance. Set if you want customize redis connect. Default connect to local.
     });
 
-    rds_client.on("connect", function () {
-        console.log("Redis connected");
-
-        if (redisConf.enabled && redisConf.ttl > 0) {                
-            // cache view data
-            console.log("start to load views: ");
-            db.getColl("template", function (data) {
-                data.forEach(function (view) {
-                    getViewData(view, am, db, search, 0);
-                });
-                // console.log("views: " + JSON.stringify(views));
-            });
-            
-            intervalObject = setInterval(function () {
-                console.log("start to load views: ");
-                db.getColl("template", function (data) {
-                    data.forEach(function (view) {
-                        getViewData(view, am, db, search, 0);
-                    });
-                    // console.log("views: " + JSON.stringify(views));
-                });
-            }, redisConf.ttl * 1000);
-        }
-    });
-
     this.search = function (req, res) {
         var keyword = req.body.keyword;
         console.log("cache search keyword: " + keyword);
@@ -63,58 +34,3 @@ module.exports = function (db, am, redisConf) {
             });
     };
 };
-
-function getViewData(view, am, db, search, offset) {
-    var restParam = {
-        server: am.server,
-        user: am.user,
-        password: am.password,
-        context: "/AssetManagerWebService/rs/",
-        "ref-link": "db/" + view['$']['sqlname'],     // "db/amLocation/126874",
-        collection: "",     // "EmplDepts",
-        param: {
-            limit: 1000,
-            offset: offset,
-            filter: view['AQL'] || "",
-            orderby: "",
-            fields: view['fields']
-        }
-    };
-
-    console.log("restParam: " + JSON.stringify(restParam));
-
-    var url = "http://${server}${context}${ref-link}${collection}";
-    var auth = 'Basic ' + new Buffer(restParam.user + ':' + restParam.password).toString('base64');
-    if (restParam.param && restParam.param['orderby'].isEmpty())
-        delete restParam.param['orderby'];
-
-    var args = {
-        path: restParam,
-        parameters: restParam.param,
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": auth
-        }
-    };
-
-    var request = client.get(url, args, function (data, response) {
-
-        view['last'] = {
-            time: Date.now(),
-            count: data.count
-        };
-        db.setColl("template", view);
-
-        data.entities.forEach(function (obj) {
-            search.index(JSON.stringify(obj), obj['ref-link'] + ":" + obj['self']);
-        });
-
-        if (data.count > restParam.param.limit + offset)
-            getViewData(view, am, db, search, restParam.param.limit + offset);
-
-    }).on('error', function (err) {
-        console.log(err);
-    });
-
-    console.log("request.options: " + JSON.stringify(request.options));
-}
--- server.js
+++ server.js
@@ -6,25 +6,14 @@ var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var am_href = process.env.AM_WEB_TIER || "http://admin:@16.186.74.164:8081";    //http://user:pass@hostname:8081
+var dbFile = { folder: 'db', json: '/template.json' };
 var redis = {
     host:       process.env.REDIS_HOST || "127.0.0.1",
     port:       process.env.REDIS_PORT || "6379",
     auth_pass:  process.env.REDIS_PASS || "",
     enabled:    process.env.REDIS_ENABLED || true,
-    ttl:        process.env.REDIS_TTL || 600  
+    ttl:        process.env.REDIS_TTL || 10
 };
-
-// initial db folder and files =================================================
-var db = require('./app/db.js');
-db.init('db', '/template.json');
-
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
-app.use(morgan('dev')); // log every request to the console
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
-
 // initial AM server
 var URL = require('url');    
 var amUrl = URL.parse(am_href);
@@ -34,13 +23,29 @@ var am = {
     password:   amUrl.auth.split(":")[1]
 }; 
 
+// initial db folder and files =================================================
+var db = require('./app/db.js');
+db.init(dbFile.folder, dbFile.json);
+
+app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+
+
 // routes ======================================================================
 require('./app/routes.js')(app, am, redis);
-
-// scheduler cache
-
-// require('./app/cache.js')(db, am);
 
 // listen (start app with node server.js) ======================================
 app.listen(port);
 console.log("App listening on port " + port);
+
+var cp = require('child_process');
+var child = cp.fork('./app/worker.js',{
+   execArgv: ['--debug=40894']
+});
+
+// Send child process some work
+child.send(JSON.stringify({am: am, db: dbFile, redis: redis}));
