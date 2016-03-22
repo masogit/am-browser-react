7cedcd2bb49d0398da5005c71aa691bd796336af
Wei Jing-Yang <jing-yang.wei@hpe.com>
2016-03-22 16:21:12 +0800

    Add code based on grommet-ferret (https://github.com/grommet/grommet-ferret).

---
+++ README.md
@@ -0,0 +1,25 @@
+To run this application, execute the following commands:
+
+  1. Install NPM modules
+    ```
+    $ cd am-browser
+    $ npm install
+    ```
+  2. Start the server
+
+    ```
+    $ cd server
+    $ gulp dev
+    ```
+
+  3. Start the UI development server
+    ```
+    $ gulp dev
+    ```
+
+  4. Check Ferret production server
+    ```
+    $ cd am-browser
+    $ gulp dist
+    $ node server/server.js
+    ```
---
+++ app/server.js
@@ -0,0 +1,60 @@
+// set up ======================================================================
+var express = require('express');
+var app = express(); 								// create our app w/ express
+
+var cors = require('cors');
+app.use(cors());
+
+var port = process.env.PORT || 8080; 				// set the port
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+var am_href = process.env.AM_WEB_TIER || "http://Admin@localhost:8081";    // http://user:pass@hostname:8081
+var dbFile = { folder: 'db', json: '/template.json' };
+var redis = {
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    port: process.env.REDIS_PORT || "6379",
+    auth_pass: process.env.REDIS_PASS || "",
+    enabled: process.env.REDIS_ENABLED || false,
+    ttl: process.env.REDIS_TTL || 1200
+};
+// initial AM server
+var URL = require('url');
+
+var am;
+if (am_href != "") {
+    am = {
+        server: URL.parse(am_href).host,
+        user: URL.parse(am_href).auth.split(":")[0],
+        password: URL.parse(am_href).auth.split(":")[1]
+    };
+}
+
+
+// initial db folder and files =================================================
+var db = require('./db.js');
+db.init(dbFile.folder, dbFile.json);
+
+app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
+app.use(morgan('dev')); // log every request to the console
+app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
+app.use(bodyParser.json()); // parse application/json
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+
+
+// routes ======================================================================
+require('./routes.js')(app, am, redis);
+
+// listen (start app with node server.js) ======================================
+app.listen(port);
+console.log("App listening on port " + port);
+
+// sub process to cache view data in Redis
+if (redis.enabled && redis.ttl > 0 && am) {
+    var cp = require('child_process');
+    var child = cp.fork('./worker.js');
+
+    // Send child process some work
+    child.send(JSON.stringify({ am: am, db: dbFile, redis: redis }));
+}
