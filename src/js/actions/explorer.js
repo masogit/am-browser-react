2299b4fb5ca796f120e7cc5c164bef8fff5c431e
Man, Shuo <shuo.man@hpe.com>
2016-04-26 16:42:50 +0800

    Display list/detail/list/detail... add two new files

---
+++ src/js/actions/explorer.js
@@ -0,0 +1,43 @@
+/**
+ * Created by mansh on 4/25/2016.
+ */
+import {HOST_NAME} from '../util/Config';
+import Rest from 'grommet/utils/Rest';
+
+function param2aql(param) {
+
+  let aql = '';
+  aql += '?limit='+param.limit;
+  aql += '&offset='+param.offset;
+  aql += '&fields='+param.fields.join(',');
+  aql += param.filter?'&filter='+param.filter:'';
+  aql += param.orderby?'&orderby='+param.orderby:'';
+
+  return encodeURI(aql);
+}
+
+export function loadRecordsByBody(body, callback) {
+
+  var fields = [];
+  body.fields.forEach(function(field) {
+    fields.push(field.sqlname);
+  });
+
+  var query = body.sqlname;
+  var param = {
+    limit: "100",
+    offset: "0",
+    filter: body.filter,
+    orderby: body.orderby,
+    fields: fields
+  };
+
+  var aql = param2aql(param);
+
+  Rest.get(HOST_NAME + '/am/db/' + query + aql).end(function (err, res) {
+    var records = res.body.entities;
+    callback(records);
+  });
+
+};
+
---
+++ src/js/components/explorer/List.js
@@ -0,0 +1,132 @@
+import React, {Component} from 'react';
+import Table from 'grommet/components/Table';
+import TableRow from 'grommet/components/TableRow';
+import * as ExplorerActions from '../../actions/explorer';
+import Split from 'grommet/components/Split';
+import Tabs from 'grommet/components/Tabs';
+import Tab from 'grommet/components/Tab';
+export default class List extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      records: [],
+      // links: [],
+      record: null
+    };
+    this._onClick.bind(this);
+    this._setState.bind(this);
+  }
+
+  componentDidMount() {
+    let self = this;
+    ExplorerActions.loadRecordsByBody(self.props.body, function (data) {
+      self._setState(data);
+    });
+  }
+
+  _onClick(record) {
+    this.setState({
+      record: record
+    });
+
+    // if (this.props.body.links && this.props.body.links.length > 0) {
+    //   var links = [];
+    //   this.props.body.links.map((link, index) => {
+    //     var linkCom = <Tab title={link.label + ' (0)'} key={index}>
+    //       <List body={this._genLinkBody(link, record)}/>
+    //     </Tab>;
+    //     links.push(linkCom);
+    //   });
+    //
+    //   this.setState({
+    //     links: links
+    //   });
+    // }
+  }
+
+  _setState(data) {
+    this.setState({
+      records: data
+    });
+  }
+
+  _genLinkBody(link, record) {
+    var body = {...link.body};
+    let AQL = link.reverse + '.PK=' + record[link.reversefield];
+    body.filter = body.filter ? body.filter + ' AND ' + AQL : AQL;
+    return body;
+  }
+
+  render() {
+    var body = this.props.body;
+    var header = body.fields.map((field, index) => {
+      return !field.PK &&
+        <th key={index}>{ field.alias ? field.alias : (field.label ? field.label : field.sqlname)}</th>;
+    });
+    var recordComponents = this.state.records.map((record, index) => {
+      return <TableRow key={index} onClick={this._onClick.bind(this, record)}>
+        <td>{record.self}</td>
+        {
+          body.fields.map((field, tdindex) => {
+            return !field.PK &&
+              <td key={tdindex}>{record[field.sqlname]}</td>;
+          })
+        }
+      </TableRow>;
+    });
+    var fields;
+    var linkTabs;
+    if (this.state.record && this.props) {
+      console.log("this.state.record: " + JSON.stringify(this.state.record));
+      fields = this.props.body.fields.map((field, index) => {
+        return <TableRow key={index}>
+          <td>{field.label}</td>
+          <td>{this.state.record[field.sqlname]}</td>
+        </TableRow>;
+      });
+      if (this.props.body.links && this.props.body.links.length > 0) {
+        linkTabs = this.props.body.links.map((link, index) => {
+          return <Tab title={link.label} key={index}>
+            <List body={this._genLinkBody(link, this.state.record)}/>
+          </Tab>;
+        });
+      }
+    }
+
+    return (
+      <Split flex="both">
+        <Table selectable={true} scrollable={true}>
+          <thead>
+          <tr>
+            <th>Self</th>
+            {header}
+          </tr>
+          </thead>
+          <tbody>
+          {recordComponents}
+          </tbody>
+        </Table>
+        {
+          this.state.record &&
+          <Tabs justify="start" initialIndex={0}>
+            <Tab title={this.props.body.label}>
+              <Table>
+                <thead>
+                <tr>
+                  <th>Field</th>
+                  <th>Value</th>
+                </tr>
+                </thead>
+                <tbody>
+                {fields}
+                </tbody>
+              </Table>
+            </Tab>
+            {linkTabs}
+          </Tabs>
+        }
+      </Split>
+    );
+  }
+}
