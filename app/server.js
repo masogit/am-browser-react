6d2a536ba55e4bc0a4f582d06e4c4ff7ae0873f3
Man, Shuo <shuo.man@hpe.com>
2016-05-12 09:56:56 +0800

    Support to configure start parameters via environment parameters

--- app/server.js
+++ app/server.js
@@ -8,34 +8,34 @@ app.use(cors());
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-var dbFile = { folder: './db', json: '/template.json' };
+var dbFile = {folder: './db', json: '/template.json'};
 var redis = {
-    host: process.env.REDIS_HOST || "127.0.0.1",
-    port: process.env.REDIS_PORT || "6379",
-    auth_pass: process.env.REDIS_PASS || "",
-    enabled: process.env.REDIS_ENABLED || false,
-    ttl: process.env.REDIS_TTL || 600
+  host: process.env.REDIS_HOST || "127.0.0.1",
+  port: process.env.REDIS_PORT || "6379",
+  auth_pass: process.env.REDIS_PASS || "",
+  enabled: process.env.REDIS_ENABLED || false,
+  ttl: process.env.REDIS_TTL || 600
 };
 
 // initial AM REST server
 var PropertiesReader = require('properties-reader');
 var properties = PropertiesReader('am-browser-config.properties');
-var rest_protocol = properties.get('rest.protocol');
-var rest_server = properties.get('rest.server');
-var rest_port = properties.get('rest.port');
-var rest_username = properties.get('rest.username');
-var rest_password = properties.getRaw('rest.password');
+var rest_protocol = process.env.AMB_REST_PROTOCOL || properties.get('rest.protocol');
+var rest_server = process.env.AMB_REST_SERVER || properties.get('rest.server');
+var rest_port = process.env.AMB_REST_PORT || properties.get('rest.port');
+var rest_username = process.env.AMB_REST_USERNAME || properties.get('rest.username');
+var rest_password = process.env.AMB_REST_PASSWORD || properties.getRaw('rest.password');
 // initial AM node server
-//var protocol = properties.get('node.protocol'); 
-var server = properties.get('node.server'); 
-var port = properties.get('node.port'); 				// set the port
-var https_port = properties.get('node.https_port');     // set the https port
+//var protocol = properties.get('node.protocol');
+var server = process.env.AMB_NODE_SERVER || properties.get('node.server');
+var port = process.env.AMB_NODE_PORT || properties.get('node.port'); 				// set the port
+var https_port = process.env.AMB_REST_HTTPS_PORT || properties.get('node.https_port');     // set the https port
 
 var am = {
-    //server: rest_protocol + "://" + rest_server + ":" + rest_port,
-	server: rest_server + ":" + rest_port,
-    user: rest_username,
-    password: rest_password
+  //server: rest_protocol + "://" + rest_server + ":" + rest_port,
+  server: rest_server + ":" + rest_port,
+  user: rest_username,
+  password: rest_password
 };
 
 // initial db folder and files =================================================
@@ -65,7 +65,7 @@ app.use(express.static(__dirname + '/public')); 		// set the static files locati
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
+app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
 app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 
@@ -74,26 +74,25 @@ require('./routes.js')(app, am, redis);
 
 // listen (start app with node server.js) ======================================
 app.listen(port, server);
-console.log("App listening HTTP on port " + port );
+console.log("App listening HTTP on port " + port);
 
 // TODO: 1. Enable this in production. 2. There are some hardcoded HTTP requests in front-end JS, which causes mixed content issues
 const KEY = 'ssl/server-key.pem';
 const CERT = 'ssl/server-cert.pem';
 try {
-    fs.statSync(KEY);
-    fs.statSync(CERT);
-    var server_options = {
-        key  : fs.readFileSync( KEY ),
-        cert : fs.readFileSync( CERT ),
-        ca   : fs.readFileSync( CERT ) //Optional for dev machine, but for production it's necessary!!!
-    };
-    https.createServer(server_options, app).listen(https_port);
-    console.log("App listening HTTPS on port " + https_port);
+  fs.statSync(KEY);
+  fs.statSync(CERT);
+  var server_options = {
+    key: fs.readFileSync(KEY),
+    cert: fs.readFileSync(CERT),
+    ca: fs.readFileSync(CERT) //Optional for dev machine, but for production it's necessary!!!
+  };
+  https.createServer(server_options, app).listen(https_port);
+  console.log("App listening HTTPS on port " + https_port);
 }
-catch (e){
-    console.warn("HTTPS is not set correctly");
+catch (e) {
+  console.warn("HTTPS is not set correctly");
 }
-
 
 
 // sub process to cache view data in Redis
