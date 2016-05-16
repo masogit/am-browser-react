3fe00c41a2488998f0333064d6f9a2827b670ad1
Man, Shuo <shuo.man@hpe.com>
2016-05-16 14:39:14 +0800

    Implementing layout edit for wall (not finish)

--- src/js/components/aql/Wall.js
+++ src/js/components/aql/Wall.js
@@ -1,10 +1,11 @@
 import React, {Component} from 'react';
 import * as AQLActions from '../../actions/aql';
-import Menu from 'grommet/components/icons/base/Menu';
+import Add from 'grommet/components/icons/base/Add';
 import {
   Anchor,
   Box,
   Chart,
+  Header,
   Title
 } from 'grommet';
 
@@ -12,9 +13,17 @@ export default class Wall extends Component {
 
   constructor() {
     super();
-    this.state={
-      aqls: []
+    this.state = {
+      aqls: [],
+      box: {
+        separator: 'all',
+        direction: 'row',
+        child: null
+      }
     };
+    this._buildBox.bind(this);
+    this._buildActions.bind(this);
+    this._addBox.bind(this);
   }
 
   componentDidMount() {
@@ -29,24 +38,65 @@ export default class Wall extends Component {
     });
   }
 
-  render() {
+  _addBox(box, direction, parent) {
+    console.log(parent);
+    box.direction = direction;
+    if (box.child) {
+      box.child.push({
+        separator: 'all',
+        direction: 'row',
+        child: null
+      });
+    } else
+      box.child = [{
+        separator: 'all',
+        direction: 'row',
+        child: null
+      }];
+    this.setState({
+      box: parent
+    });
+  }
 
+  _buildBox(box, parent) {
+    console.log(box);
+    return (<Box {...box}>
+      {this._buildActions(box, parent)}
+      {
+        box.child &&
+        box.child.map((child) => {
+          return this._buildBox(child, parent);
+        })
+      }
+    </Box>);
+  }
+
+  _buildActions(box, parent) {
+    return (<Header>
+      <Anchor href="#" icon={<Add />} label="Row" onClick={this._addBox.bind(this, box, 'row', parent)}/>
+      <Anchor href="#" icon={<Add />} label="Column" onClick={this._addBox.bind(this, box, 'column', parent)}/>
+      <Anchor href="#" icon={<Add />} label="AQL"/>
+    </Header>);
+  }
+
+  render() {
+    var box = this.state.box;
     return (
-      <Box direction="column">
-      <Box align="end">
-        <Anchor href="#" icon={<Menu />} />
-      </Box>
-      <Box align="top" justify="center" direction="row" pad="medium">
-        {
-          this.state.aqls.map((aql, i) => {
-            return aql.chart &&
-              <Box direction="column" pad="small" separator="horizontal">
-                <Title>{aql.name}</Title>
-                <Chart key={i} {...aql.chart} />
-              </Box>;
-          })
-        }
-      </Box>
+      <Box direction="column" pad="medium">
+        <Box separator="all">
+          {this._buildBox(box, box)}
+        </Box>
+        <Box justify="center" direction="row" pad="medium">
+          {
+            this.state.aqls.map((aql, i) => {
+              return aql.chart &&
+                <Box direction="column" pad="small" separator="horizontal">
+                  <Title>{aql.name}</Title>
+                  <Chart key={i} {...aql.chart} />
+                </Box>;
+            })
+          }
+        </Box>
       </Box>
     );
   }
