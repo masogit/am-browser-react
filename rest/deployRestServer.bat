a03b7cf06b1cd13e5c0054e5b64dd5b3d2fe4a3f
xiaoliang.bu <xiaoliang.bu@hpe.com>
2016-05-25 14:19:57 +0800

    [Build] Run Rest server as tomcat service.

---
+++ rest/deployRestServer.bat
@@ -0,0 +1,4 @@
+@echo off
+set SCRIPTPATH=%~dp0
+call "%SCRIPTPATH%\deploy\deploy.bat" /x64 websvc\package.properties
+pause
---
+++ rest/registerRestService.bat
@@ -0,0 +1,3 @@
+@echo off
+set SCRIPTPATH=%~dp0
+"%SCRIPTPATH%\startRestServiceCmd.bat" install am-browser-rest-service
