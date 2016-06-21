3030384d43d1d75b93cc193d7267b784adb73d5e
Man, Shuo <shuo.man@hpe.com>
2016-06-21 14:41:47 +0800

    Remove unused public folder and configure

--- app/server.js
+++ app/server.js
@@ -24,7 +24,6 @@ var isDebug = process.env.AMB_NODE_DEBUG || config.isDebug;
 var enable_csrf = process.env.AMB_NODE_CSRF || config.enable_csrf;
 
 app.use(compression());
-app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 //app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
 app.use(bodyParser.json()); // parse application/json
