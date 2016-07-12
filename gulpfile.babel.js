f6ab1f87a3ed23abc0cefd6d50b57e4c78a90016
xiaoliang.bu <xiaoliang.bu@hpe.com>
2016-07-12 13:57:04 +0800

    [Build] colon replacement.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -297,7 +297,7 @@ gulp.task('download-ws', ['parse-metadata-xml'], function () {
   if (config.Release_CPP) {
 	  x64_version_info = 'MAIN-' + config.Release_CPP + + '-' + config.Classifier + '.zip';
   }
-  x64_url = config.Nexus + 'com/hp/am/cpp/binary/MAIN-SNAPSHOT/binary-' +　x64_version_info；
+  x64_url = config.Nexus + 'com/hp/am/cpp/binary/MAIN-SNAPSHOT/binary-' +　x64_version_info;
   console.log('x64 binary url is : ' + x64_url);
   var openssl_json = require('./rest/downloads/openssl/json/maven-metadata.json');
   var openssl_url = config.Nexus3rd + 'com/hp/am/3rd/openssl/'
