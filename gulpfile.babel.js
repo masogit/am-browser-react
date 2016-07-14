8ef7467c7732d3605115ccf1141cd3c95967aecd
xiaoliang.bu <xiaoliang.bu@hpe.com>
2016-07-14 11:11:04 +0800

    [Build] Changes for version build.

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -212,8 +212,8 @@ gulp.task('copy-temp', ['dist', 'clean-gen'], function () {
 gulp.task('gen', ['copy-temp'], function () {
   console.log('Generate am-browser.zip from temp folder');
   //var timestamp = Math.floor(new Date().getTime()/1000);
-  var build = version.stage ? version.number + '-' + timestamp + '_' + version.stage : '';
-  var name = 'am-browser' + '-' + build + '.zip';
+  var build = version.stage ? '-' + timestamp + '_' + version.stage : '';
+  var name = 'am-browser' + '-' + version.number + build + '.zip';
   // generate am-browser.zip from temp folder
   return gulp.src('./gen/temp/**')
       .pipe(zip(name))
@@ -399,8 +399,8 @@ gulp.task('gen-ws', ['gen-ws-conf'], function () {
   console.log('Generate am-browser-rest.zip from temp folder');
   // generate am-browser-rest.zip from temp folder
   //var timestamp = Math.floor(new Date().getTime()/1000);
-  var build = version.stage ? version.number + '-' + timestamp + '_' + version.stage : '';
-  var name = 'am-browser-rest' + '-' + build + '.zip';
+  var build = version.stage ? '-' + timestamp + '_' + version.stage : '';
+  var name = 'am-browser-rest' + '-' + version.number + build + '.zip';
   return gulp.src('./rest/gen/temp/**')
       .pipe(zip(name))
       .pipe(gulp.dest('./rest/gen'));
