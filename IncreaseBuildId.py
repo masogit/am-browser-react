4d42ded4ce36db2413d16f92f81dd4f911658e7e
Lin, Li <llin@hpe.com>
2016-09-21 16:43:49 +0800

    add increase build id and tag script

---
+++ IncreaseBuildId.py
@@ -0,0 +1,25 @@
+#! /user/bin/python2
+import json
+import subprocess
+
+
+versionConfig = './version.json'
+
+# read version config file and +1
+with open(versionConfig, 'r') as f:
+	version = json.load(f)
+	print version
+	currentId = str(int(version['id']) + 1)
+if currentId:
+	with open(versionConfig, 'w') as f:
+		json.dump({"number":version['number'], "id":currentId}, f)
+tagName = version['number'] + "-" + currentId
+print "current build tag is " + tagName
+
+# tag or re-tag current build number 
+isTagExist = subprocess.check_output(["git", "tag", "-l", tagName])
+if isTagExist:
+	subprocess.call(["git", "tag", "-d", tagName])
+	subprocess.call(["git", "push", "--delete", "origin", tagName])
+subprocess.call(["git", "tag", "-a", tagName, "-m", "AM Browser tagName"])
+subprocess.call(["git", "push", "origin", tagName])
