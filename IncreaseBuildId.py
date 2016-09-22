2bbaddb0975935db13e2654872f573c4d32aa4ed
Lin, Li <llin@hpe.com>
2016-09-22 10:52:21 +0800

    push branch and tag together

--- IncreaseBuildId.py
+++ IncreaseBuildId.py
@@ -8,22 +8,23 @@ versionConfig = './version.json'
 gitBranch = 'master'
 
 # read version config file
+
 with open(versionConfig, 'r') as f:
 	version = json.load(f)
-	print version
+	print "--------------last build id is " +  version['id'] + "--------------"
 
 if version['id']:
 	currentId = str(int(version['id']) + 1)
 	tagName = version['number'] + "-" + currentId
-	commitComment = "current build tag is " + tagName
-	print commitComment
+	commitComment = "current build id is " + tagName
+	print "--------------" + commitComment + "--------------"
 
-	# tag or re-tag current build number and push to git server
+	# tag or re-tag current build number
 	isTagExist = subprocess.check_output(["git", "tag", "-l", tagName])
 	if isTagExist:
 		subprocess.call(["git", "tag", "-d", tagName])
 		subprocess.call(["git", "push", "--delete", "origin", tagName])
-	subprocess.call(["git", "tag", "-a", tagName, "-m", "AM Browser tagName"])
+	subprocess.call(["git", "tag", "-a", tagName, "-m", "AM Browser"])
 	
 	# checkout branch and modify version config file
 	subprocess.call(["git", "checkout", gitBranch])
@@ -31,7 +32,7 @@ if version['id']:
 		json.dump({"number":version['number'], "id":currentId}, f)
 	subprocess.call(["git", "add", versionConfig])
 	subprocess.call(["git", "commit", "-m", commitComment])
-	subprocess.call(["git", "push", "--tags"])
+	subprocess.call(["git", "push", "origin", gitBranch, tagName])
 else:
 	print "can not fetch version id, exit"
 	sys.exit(1)
