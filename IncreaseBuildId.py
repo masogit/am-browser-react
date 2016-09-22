95a6d3c62ba851e94e4cff7a8bb78a0452fa585a
Lin, Li <llin@hpe.com>
2016-09-22 13:59:42 +0800

    fix version script bug

--- IncreaseBuildId.py
+++ IncreaseBuildId.py
@@ -15,23 +15,26 @@ with open(versionConfig, 'r') as f1:
 if version['id']:
 	currentId = str(int(version['id']) + 1)
 	tagName = version['number'] + "-" + currentId
-	commitComment = "[Auto build]:current build id is " + tagName
+	commitComment = "[Auto Build]:current build id is " + tagName
 	print "--------------" + commitComment + "--------------"
-
-	# tag or re-tag current build number
-	isTagExist = subprocess.check_output(["git", "tag", "-l", tagName])
-	if isTagExist:
-		subprocess.call(["git", "tag", "-d", tagName])
-		subprocess.call(["git", "push", "--delete", "origin", tagName])
-	subprocess.call(["git", "tag", "-a", tagName, "-m", "AM Browser"])
 	
-	# checkout branch and modify version config file
-	subprocess.call(["git", "checkout", gitBranch])
+	# modify version config file
 	with open(versionConfig, 'w') as f2:
 		json.dump({"number":version['number'], "id":currentId}, f2)
 	subprocess.call(["git", "add", versionConfig])
 	subprocess.call(["git", "commit", "-m", commitComment])
-	subprocess.call(["git", "push", "origin", gitBranch, tagName])
+	
+	# tag or re-tag current build number
+	isTagExist = subprocess.check_output(["git", "tag", "-l", tagName])
+		if isTagExist:
+		subprocess.call(["git", "tag", "-d", tagName])
+		subprocess.call(["git", "push", "--delete", "origin", tagName])
+	subprocess.call(["git", "tag", "-a", tagName, "-m", "AM Browser"])
+
+	# push both commit and tag
+	pushRefs = "HEAD:" + gitBranch
+	subprocess.call(["git", "push", "origin", pushRefs, tagName])
+
 else:
 	print "-------------can not fetch version id, exit--------------"
 	sys.exit(1)
