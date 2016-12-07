90a33d9ebd6ecadfbff8c0f0d10a15253e9f1007
Huang Ling <ling.huang3@hpe.com>
2016-12-07 11:49:39 +0800

    [Refine Code][Commons] add index.js for commons

---
+++ src/js/components/commons/index.js
@@ -0,0 +1,46 @@
+/**
+ * Created by huling on 12/7/2016.
+ */
+import ActionLabel from './ActionLabel';
+import ActionTab from './ActionTab';
+import AlertForm from './AlertForm';
+import AMSideBar from './AMSideBar';
+import AMSideToolBar from './AMSideToolBar';
+import ComponentBase from './ComponentBase';
+import ContentPlaceHolder from './ContentPlaceHolder';
+import EditLayer from './EditLayer';
+import EmptyIcon from './EmptyIcon';
+import Graph from './Graph';
+import GroupList from './GroupList';
+import GroupListItem from './GroupListItem';
+import Legend from './Legend';
+import Legend_Chart from './Legend_Chart';
+import Legend_Distribution from './Legend_Distribution';
+import Legend_Meter from './Legend_Meter';
+import PDFWidgets from './PDFWidgets';
+import SearchInput from './SearchInput';
+import Textarea from './Textarea';
+import {UploadWidget} from './Widgets';
+
+export {
+  ActionLabel,
+  ActionTab,
+  AlertForm,
+  AMSideBar,
+  AMSideToolBar,
+  ComponentBase,
+  ContentPlaceHolder,
+  EditLayer,
+  EmptyIcon,
+  Graph,
+  GroupList,
+  GroupListItem,
+  Legend,
+  Legend_Chart,
+  Legend_Distribution,
+  Legend_Meter,
+  PDFWidgets,
+  SearchInput,
+  Textarea,
+  UploadWidget
+};
